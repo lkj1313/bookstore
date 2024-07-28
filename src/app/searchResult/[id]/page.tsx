4ee2032183e2a4d8f41d691dcc7602e1a02338be
unknown <smm45108@gmail.com>
2024-07-28 22:40:17 +0900

    무한스크롤 빼고 다시 페이지네이션 표시

--- src/app/searchResult/[id]/page.tsx
+++ src/app/searchResult/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Link from "next/link";
 
 interface PageProps {
@@ -23,109 +23,160 @@ interface Book {
 }
 
 const SearchResultPage = (props: PageProps) => {
-  // 상태 변수를 정의합니다.
-  const [books, setBooks] = useState<Book[]>([]); // 검색된 책 목록
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 번호
-  const [loading, setLoading] = useState(false); // 로딩 상태
-  const [hasMore, setHasMore] = useState(true); // 추가 데이터 여부
+  const [books, setBooks] = useState<Book[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+  const [showContent, setShowContent] = useState(false);
 
-  // IntersectionObserver를 관리하는 useRef 훅
-  const observer = useRef<IntersectionObserver | null>(null);
-
-  // 마지막 책 요소에 대한 콜백 함수
-  const lastBookElementRef = useCallback(
-    (node: any) => {
-      if (loading) return; // 로딩 중이면 아무 작업도 하지 않음
-      if (observer.current) observer.current.disconnect(); // 기존 옵저버 연결 해제
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setCurrentPage((prevPage) => prevPage + 1); // 현재 페이지를 증가시킴
-        }
-      });
-      if (node) observer.current.observe(node); // 새로운 노드를 옵저버에 등록
-    },
-    [loading, hasMore]
-  );
-
-  const query = decodeURIComponent(props.params.id); // URL에서 검색 쿼리를 디코드
+  const query = decodeURIComponent(props.params.id);
 
   useEffect(() => {
-    const fetchBooks = async () => {
-      setLoading(true); // 로딩 상태를 true로 설정
+    const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://bookstore-phi-five.vercel.app/search/book?query=${query}&display=50&page=${currentPage}`
+          `https://bookstore-phi-five.vercel.app/search/book?query=${query}&display=100`
         );
         const items = response.data.items;
-        if (items.length === 0) {
-          setHasMore(false); // 더 이상 데이터가 없으면 hasMore를 false로 설정
-        } else {
-          setBooks((prevBooks) => [...prevBooks, ...items]); // 기존 책 목록에 새로 가져온 책을 추가
-        }
+        setBooks(items);
+        setShowContent(true); // 데이터를 성공적으로 가져온 후 showContent를 true로 설정
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
-      setLoading(false); // 로딩 상태를 false로 설정
     };
 
-    fetchBooks(); // 데이터를 가져오는 함수 호출
-  }, [query, currentPage]); // 검색 쿼리나 현재 페이지가 변경될 때마다 실행
+    fetchData();
+  }, [query]);
+
+  const handlePageClick = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) {
+      setShowScrollToTop(true);
+    } else {
+      setShowScrollToTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const itemsPerPage = 24;
+  const totalPages = Math.ceil(books.length / itemsPerPage);
+  const paginationSize = 5;
+  const startPage =
+    Math.floor((currentPage - 1) / paginationSize) * paginationSize + 1;
+  const endPage = Math.min(startPage + paginationSize - 1, totalPages);
 
   return (
-    <div>
+    <div className={`fade-in ${showContent ? "show" : ""}`}>
       <Container style={{ marginTop: "30px" }}>
         <div className="row">
-          {books.map((book, index) => (
-            <div
-              className="col-lg-4 col-md-6 col-sm-12 bookListBox"
-              key={index}
-              ref={books.length === index + 1 ? lastBookElementRef : null} // 마지막 책 요소에 대한 ref 설정
-            >
+          {books
+            .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+            .map((book, index) => (
               <div
-                className="card"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  padding: "10px",
-                  border: "0.3px solid #D2E1FF",
-                  alignItems: "center",
-                  marginBottom: "10px",
-                  overflow: "hidden",
-                }}
+                className="col-lg-4 col-md-6 col-sm-12 bookListBox"
+                key={index}
               >
-                <Link
-                  href={`/book/${book.isbn}`}
-                  style={{ height: "65%", width: "100%" }}
-                >
-                  <img
-                    style={{ height: "100%", width: "100%" }}
-                    src={book.image}
-                    className={`card-img-top img-darken-on-hover `}
-                    alt={book.title}
-                  />
-                </Link>
                 <div
-                  className="card-body"
+                  className="card"
                   style={{
-                    textAlign: "center",
+                    width: "100%",
+                    height: "100%",
+                    padding: "10px",
+                    border: "0.3px solid #D2E1FF",
+                    alignItems: "center",
+                    marginBottom: "10px",
+                    overflow: "hidden",
                   }}
                 >
-                  <h5
-                    className="card-title, rato-paragraph"
-                    style={{ marginTop: "10px" }}
+                  <Link
+                    href={`/book/${book.isbn}`}
+                    style={{ height: "65%", width: "100%" }}
                   >
-                    {book.title}
-                  </h5>
-                  <p style={{ fontWeight: "normal" }}>
-                    {book.author.replace(/\^/g, ", ")}
-                  </p>
-                  <span>₩{parseInt(book.discount).toLocaleString()}</span>
+                    <img
+                      style={{ height: "100%", width: "100%" }}
+                      src={book.image}
+                      className={`card-img-top img-darken-on-hover `}
+                      alt={book.title}
+                    />
+                  </Link>
+                  <div
+                    className="card-body"
+                    style={{
+                      textAlign: "center",
+                    }}
+                  >
+                    <h5
+                      className="card-title rato-paragraph"
+                      style={{ marginTop: "10px" }}
+                    >
+                      {book.title}
+                    </h5>
+                    <p style={{ fontWeight: "normal" }}>
+                      {book.author.replace(/\^/g, ", ")}
+                    </p>
+                    <span>₩{parseInt(book.discount).toLocaleString()}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
-        {loading && <p>Loading...</p>} {/* 로딩 중일 때 표시 */}
+        {totalPages > 1 && (
+          <nav
+            aria-label="Page navigation example"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <ul className="pagination">
+              {Array.from(
+                { length: endPage - startPage + 1 },
+                (_, i) => i + startPage
+              ).map((pageNum) => (
+                <li
+                  className={`page-item ${
+                    pageNum === currentPage ? "active" : ""
+                  }`}
+                  key={pageNum}
+                >
+                  <a
+                    className="page-link"
+                    href={`#`}
+                    onClick={() => handlePageClick(pageNum)}
+                  >
+                    {pageNum}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+        {showScrollToTop && (
+          <Button
+            onClick={scrollToTop}
+            style={{
+              position: "fixed",
+              bottom: "20px",
+              right: "20px",
+              zIndex: 1000,
+              opacity: 0.7,
+              transition: "opacity 0.5s",
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.opacity = "1")}
+            onMouseLeave={(e) => (e.currentTarget.style.opacity = "0.7")}
+          >
+            Scroll to Top
+          </Button>
+        )}
       </Container>
     </div>
   );
